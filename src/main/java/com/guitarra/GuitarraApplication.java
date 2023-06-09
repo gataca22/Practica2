9e88433bde0f85167f8b4287b24d4edb3b762b48
GabrielTapia <gabrieltapia04@gmail.com>
2023-06-09 00:00:15 -0600

    Practica2_GabrielTapiaCalvo

---
+++ src/main/java/com/guitarra/GuitarraApplication.java
@@ -0,0 +1,13 @@
+package com.guitarra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guitarra/GuitarraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guitarra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
